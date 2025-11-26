f36e1dda84dec1133dcc3ef56001f8e86cfb6877
MykytaRomaniuk-STUDE <MykytaRomaniuk-STUDE@S-6GY36V3>
2025-11-26 12:35:12 +0000

    Add project files.

---
+++ RaindropFall/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RaindropFall
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RaindropFall/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RaindropFall")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RaindropFall.Pages")]
